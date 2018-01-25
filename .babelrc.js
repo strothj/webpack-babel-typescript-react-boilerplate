d97f658a8d0b4993545020be129eaae5c64add8b
jason@jasons.io <jason@jasons.io>
2018-01-24 20:03:04 -0500

    chore: make react component names available during development

--- .babelrc.js
+++ .babelrc.js
@@ -3,7 +3,11 @@ const env = process.env.BABEL_ENV || process.env.NODE_ENV;
 module.exports = {
   presets: [
     "@babel/preset-typescript",
-    "@babel/preset-react",
+    [
+      "@babel/preset-react",
+      // Adds __self attribute to JSX which React will use for some warnings
+      { development: env === "development" || env === "test" },
+    ],
     [
       "@babel/preset-env",
       {
